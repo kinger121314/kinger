59d2e976fceb475b7871c8637664acae3ccffd62
kinger121314 <1908001909@qq.com>
2014-03-09 14:09:50 +0800

    修改增加图标

--- gen/com/whr/taskmanager/R.java
+++ gen/com/whr/taskmanager/R.java
@@ -21,8 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int active=0x7f020000;
+        public static final int bell=0x7f020001;
+        public static final int coord=0x7f020002;
+        public static final int garbage=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int modify=0x7f020005;
+        public static final int no_active=0x7f020006;
+        public static final int shake=0x7f020007;
+        public static final int star=0x7f020008;
+        public static final int voice=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
